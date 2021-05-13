b9c5a1f7de2d42335b43d53035072950a13ca775
Shin-Yong-Jae <sms3284@naver.com>
2021-05-14 00:14:17 +0900

    프로그래머스-디스크컨트롤러(heap)

---
+++ 디스크컨트롤러(heap).cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int solution(vector<vector<int>> jobs)
+{ 
+    int answer = 0;
+    sort(jobs.begin(), jobs.end());
+    priority_queue<pair<int, int>, vector<pair<int, int>>,
+    greater<pair<int, int>>> pq;
+    int idx = 0, time = 0;
+    while (idx < jobs.size() || !pq.empty())
+    {
+        if(idx < jobs.size() && time >= jobs[idx][0])
+        {
+            pq.push(make_pair(jobs[idx][1], jobs[idx][0]));
+            idx++;
+            continue;
+        }
+        if(!pq.empty()) { time += pq.top().first; // 완료된 시간
+                         answer += time - pq.top().second; // 기다린 시간 더해줌
+                         pq.pop();
+                        }
+        else
+        {
+            time = jobs[idx][0]; 
+        }
+    }
+    return answer / jobs.size(); 
+}
